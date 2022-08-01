4823039e4bc6b5528d7b9a260974b2721090164b
iamamirhe <90977026+iamamirhe@users.noreply.github.com>
2022-08-01 20:06:07 +0430

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let course_button = document.querySelector("#button_13");
+    course_button.addEventListener("click", function() {
+        window.open("https://13mag.ir", '_blank');
+    })
+});
